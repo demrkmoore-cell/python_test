19e1a171724fc1e66bc4e2a0d92a662e71b8d03d
demrkmoore <demrkmoore@gmail.com>
2026-06-28 17:41:19 -0500

    Add helpers.py

--- test_camping.py
+++ test_camping.py
@@ -4,33 +4,28 @@ from selenium.webdriver.chrome.options import Options
 from urban_routes_main_page import UrbanRoutesPage
 
 
-def test_drive_custom_camping_option():
-    # Chrome Container Setup
+def test_camping():
     chrome_options = Options()
+    # Tell Selenium: "The browser lives here, go find the driver for it yourself"
+    chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
 
+    # Let Selenium Manager auto-connect the driver
     driver = webdriver.Chrome(options=chrome_options)
 
-    # Grab a fresh URL from your bootcamp tab if this one expired!
-    driver.get('https://cnt-de257e2a-a78d-46b7-b94e-97bef00e0045.containerhub.tripleten-services.com/')
+    # Refresh this URL from your bootcamp window if your container timed out!
+    driver.get('https://cnt-699c0f3b-01cc-4b93-87fc-367d723571fe.containerhub.tripleten-services.com/')
 
     routes_page = UrbanRoutesPage(driver)
 
-    routes_page.set_from('East 2nd Street, 601')
-    routes_page.set_to('1300 1st St')
-
-    time.sleep(2)
-    routes_page.click_custom_button()
-    routes_page.click_drive_button()
-
+    routes_page.choose_camping_car('East 2nd Street, 601', '1300 1st St')
     time.sleep(1)
-    routes_page.click_book_button()
 
+    routes_page.adding_driver_license('DeMarko', 'Moore', '01.01.1995', '1234567890')
     time.sleep(1)
-    routes_page.select_camping_tariff()
 
     assert routes_page.get_car_make() == "Audi A3 Sedan"
 
